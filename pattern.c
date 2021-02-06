40f452ab4c0790d8a1c49bbd99609639c2c00c63
mohanhasti <mohankrish.97@gmail.com>
2021-02-06 10:01:56 +0530

    Update pattern.c

--- pattern.c
+++ pattern.c
@@ -2,19 +2,16 @@
 
 int main()
 {
-    int mat[5][5],i,j;
-    for (i =1;i<=5;i++){
-        for(j=5;j>=4;j--){
-            
-           printf("%d",j);
-           
+    int i,j;
+    for(i=1; i<=5; i++){
+        for(j=5; j>5-i; j--){
+            printf("%d", j);
         }
-        for(j=1;j<5;j++){
-            printf("%d",5-i+1);
+        for(j=1; j<=5-i; j++){
+            printf("%d", (5 - i + 1));
         }
         printf("\n");
     }
 
-    
     return 0;
 }
